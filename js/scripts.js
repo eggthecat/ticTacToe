50989becf535cf426d05a170690375aff5619bf7
dominic montelongo and hannah melendy <hannah@>
2019-06-19 16:11:41 -0700

    Add new instance of game in index.html, scripts.js, and styles.css while updating README.md

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,58 @@ function Game(player1, player2, turn) {
 // this.wins = wins
 }
 
+//clickColor
+// if(#topLeft).click() {
+//
+// }
+
+Game.prototype.switch = function () {
+  if(turn[0]) {
+    this.turn = player2;
+  } else {
+    this.turn = player1;
+  }
+}
+
+// Game.prototype.winCheck = function () {
+//   if(n
+//
+// }
+
+
+
 
 
 //UI logic
 
 $(function() {
 
-  $(".jumbotron").click(event) {
+  $(".jumbotron").click(function (event) {
     event.preventDefault();
 
+    $("#topLeft").click(function() {
+      $(".grid-item").addClass("red-background");
+    });
 
 
-  }
+    // $(player1).click(function() {
+    //   $(" ").addClass("Red-background");
+    // });
+    // $(player2).click(function() {
+    //   $(" ").addClass("Blue-background");
+    // });
+    $("#restartBtn").click(function() {
+      $(".grid-container").clear();
+    });
+
+    // var turn = player1;
+    // turn.switch();
+    // console.log(turn)
+    var ourGame = new Game("Dom", "Hannah", ["x"]);
+    console.log(ourGame.player1);
+    console.log(ourGame.player2);
+  });
+
 
 
 });
