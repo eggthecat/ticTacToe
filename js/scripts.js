88994b8ea3b07cbdbb71a39c3db81530e63a3b0a
dominic montelongo and hannah melendy <dominic@>
2019-06-19 11:40:55 -0700

    add constructor for game

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
 //business logic
 
-/*
-Constructors:
-game
-keys:
-player1: name,
-player2: name,
-score:
 
+function Game(player1, player2, turn) {
+  this.player1 = player1,
+  this.player2 = player2,
+  this.turn = ['x','o']
+// this.wins = wins
+}
 
-keep track of whose turn
-*/
 
 
 //UI logic
+
+$(function() {
+
+  $(".jumbotron").click(event) {
+    event.preventDefault();
+
+
+
+  }
+
+
+});
