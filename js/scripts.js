00934a8d6b64e8707e8f6697e6629309148eb55c
dominic montelongo and hannah melendy <hannah@>
2019-06-20 16:12:27 -0700

    add ability to input names for player1 and player2

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,11 @@
 //business logic
 
-
 function Game(player1, player2, turn) {
   this.player1 = player1,
   this.player2 = player2,
   this.turn = ['x','o']
 // this.wins = wins
 }
-//clickColor
-// if(#topLeft).click() {
-//
-// }
 
 Game.prototype.switch = function () {
   if(turn[0]) {
@@ -25,6 +20,7 @@ function Players(player1, player2, playTurn) {
 	this.player2 = 'o';
 	this.playTurn = 'x';
 }
+var plays2 = new Players( 'Thank you', 'Todd', 'o');
 
     //change player turn
 Players.prototype.changeTurn = function() {
@@ -35,40 +31,24 @@ Players.prototype.changeTurn = function() {
     }
 }
 
+var ourGame = new Players(player1Input, player2Input);
 
+Players.prototype.whoseHere = function () {
+  console.log(player1Input);
+  console.log(player2Input);
+}
 
-var plays2 = new Player( 'Thank you', 'Todd', 'o');
-
-
-
-// Game.prototype.winCheck = function () {
-//   if(n
 //
+//
+// Game.prototype.turnColor = function () {
+//
+//   $(".grid-container").click(function() {
+//     $(".grid-item").addClass("red-background");
+//   });
+//   $(".grid-container").click(function() {
+//     $(".grid-item").addClass("blue-background");
+//   });
 // }
-
-// testPlayer.mark(); // returns "X"
-
-// var board = new Board();
-// var testSpace = board.find(1, 2); // board.find(1,2) returns a Space object
-//
-// testSpace.xCoordinate(); // returns 1
-// testSpace.yCoordinate(); // returns 2
-//
-// testSpace.mark(testPlayer);
-// testSpace.markedBy(); // returns testPlayer or "X"
-//
-// board.gameOver()
-
-Game.prototype.turnColor = function () {
-
-  $(".grid-container").click(function() {
-    $(".grid-item").addClass("red-background");
-  $(".grid-container").click(function() {
-    $(".grid-item").addClass("blue-background");
-  });
-});
-
-
 
 //UI logic
 
@@ -82,27 +62,19 @@ $(function() {
       var player1Input = $("#player1").val();
       console.log(player1Input);
       var player2Input = $("#player2").val();
-      console.log(player2Input)
+      console.log(player2Input);
     });
 
     $("#topLeft").click(function() {
       $(".grid-item").turnColor();
     });
 
-
+    ourGame.whoseHere();
 
     $("#restartBtn").click(function() {
       $(".grid-container").clear();
     });
 
-    // var turn = player1;
-    // turn.switch();
-    // console.log(turn)
-    var ourGame = new Game("Dom", "Hannah", ["x"]);
-    console.log(ourGame.player1);
-    console.log(ourGame.player2);
+
   });
-
-
-
 });
